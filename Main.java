77902937288b045eccc9a5eb73f139ae8f51c84a
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-02-13 13:54:31 +0000

    2/13

--- Main.java
+++ Main.java
@@ -37,7 +37,34 @@ public class Main {
       // Can also store value in a new variable
       int worstSpot = minefield[0][1];
 
+      // NESTED Loops are necessary to traverse 2D arrays
+      // ENHANCED FOR-EACH LOOP to "visit" all items in order
+      // OUTER loop iterates through each row
+      // IMPORTANT: Data type of a ROW is a 1D Array
+      for (String[] row : seatingChart) {
+         // INNER loop iterates throough column values
+         // "for every string item in the row array"
+         for (String col : row) {
+            System.out.print(col + " ");
+         }
+         System.out.println(); // line break between rows
+      }
 
+      // Cannot use for-each if you want to MODIFY VALUES
+      // or iterate in a different order
+      // STANDARD ("indexed") FOR LOOP
+      // array.length represents NUMBERS OF ROWS in the 2D array
+      for (int row=0; row < minefield.length; row++) {
+         // array[0].length represents NUMBER OF COLUMNS in a row
+         for (int col=0; col < minefield[0].length; col++) {
+            // if spot is a "bomb" (negative num), replace it
+            if (minefield[row][col] < 0) {
+               minefield[row][col] = minefield[row][col] * -1;
+            }
+            System.out.print("[" + minefield[row][col] + "]");
+         }
+         System.out.println();
+      }
 
 
 
